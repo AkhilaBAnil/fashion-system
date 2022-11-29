8c3e40114c5dcabd4ad1b273823f5734ce4f5a06
akhila <akhilakalyani2000@gmail.com>
2022-11-29 07:54:47 +0530

    second commit

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ feature_list = []
 for file in tqdm(filenames):
     feature_list.append(extract_features(file,model))
 
-pickle.dump(feature_list,open('embeddings.pkl','wb'))
-pickle.dump(filenames,open('filenames.pkl','wb'))
+pickle.dump(feature_list,open("embeddings.pkl","wb"))
+pickle.dump(filenames,open("filenames.pkl","wb"))
